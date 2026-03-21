b4403d65e43b8a84a7bf3bf90964f7137df2d4c3
Arsh Zahed <arshzahed@meta.com>
2026-03-20 17:40:21 -0700

    Add dictionary BUILTIN_CALLABLES to lookup_callables

--- torch/_dynamo/trace_rules.py
+++ torch/_dynamo/trace_rules.py
@@ -3972,6 +3972,10 @@ def is_torch(filename: str) -> bool:
 Main entry point for looking up the trace rule (the Dynamo variable) for a given callable object.
 """
 
+BUILTIN_CALLABLES = {
+    dict: DictBuiltinVariable,
+    iter: IterBuiltinVariable
+}
 
 def lookup_callable(obj: Callable[..., Any]) -> type[VariableTracker] | None:
     if not hashable(obj):
@@ -3983,10 +3987,8 @@ def lookup_callable(obj: Callable[..., Any]) -> type[VariableTracker] | None:
         return TorchInGraphFunctionVariable
     if is_polyfilled_callable(obj):
         return PolyfilledFunctionVariable
-    if obj is dict:
-        return DictBuiltinVariable
-    if obj is iter:
-        return IterBuiltinVariable
+    if obj in BUILTIN_CALLABLES:
+        return BUILTIN_CALLABLES[obj]
     if is_builtin_callable(obj):
         return BuiltinVariable
     return None
